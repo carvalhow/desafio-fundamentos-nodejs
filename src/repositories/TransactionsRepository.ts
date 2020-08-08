04f6474bb3733b346baef5936e229679e65a40dc
Wesley Carvalho <wesleycarvalho.o@hotmail.com>
2020-08-08 16:31:47 -0300

    Add creation and list routes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactions = this.all();
+    const income: number = transactions.reduce(
+      (acc: number, curr) => (curr.type === 'income' ? acc + curr.value : acc),
+      0,
+    );
+
+    const outcome: number = transactions.reduce(
+      (acc: number, curr) => (curr.type === 'outcome' ? acc + curr.value : acc),
+      0,
+    );
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction: Transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,35 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!title) {
+      throw Error('A title must be provided.');
+    }
+
+    if (!value || value <= 0) {
+      throw Error('Value must be provided and should be > 0 ');
+    }
+
+    if (!type || (type !== 'income' && type !== 'outcome')) {
+      throw Error('Type should be either Income or Outcome');
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      const balanceAfterOutcome: number = balance.total - value;
+
+      if (balanceAfterOutcome < 0) {
+        throw Error('Invalid transaction: your balance would be negative.');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
